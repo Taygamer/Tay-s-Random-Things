316bd16bf140f207c098600b9fb7730dbb775170
Taygamer <tidge@DESKTOP-TAY.home>
2024-09-08 20:40:10 +0100

    Changed Values, Export Mod For Distrubtal Use

--- src/main/java/net/mcreator/taysrandomthings/entity/TayEntity.java
+++ src/main/java/net/mcreator/taysrandomthings/entity/TayEntity.java
@@ -168,10 +168,11 @@ public class TayEntity extends Monster {
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
 		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.5);
-		builder = builder.add(Attributes.MAX_HEALTH, 1000);
+		builder = builder.add(Attributes.MAX_HEALTH, 800);
 		builder = builder.add(Attributes.ARMOR, 15);
-		builder = builder.add(Attributes.ATTACK_DAMAGE, 50);
+		builder = builder.add(Attributes.ATTACK_DAMAGE, 40);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 50);
+		builder = builder.add(Attributes.KNOCKBACK_RESISTANCE, 0.1);
 		builder = builder.add(Attributes.ATTACK_KNOCKBACK, 0.5);
 		return builder;
 	}
